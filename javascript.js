ffc46115a39e78f7f4981d7618990eca0d5167fb
Anjan Chakraborty <25178845+anjan-kr-chakraborty@users.noreply.github.com>
2025-07-26 18:02:51 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var navLinks = document.querySelectorAll('.nav-link');
-    var menuToggle = document.getElementById('navbarNav');
-    var bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false});
-    navLinks.forEach(function(navLink) {
-        navLink.addEventListener('click', function() {
-            if (window.innerWidth < 992) { // Bootstrap's lg breakpoint
-                bsCollapse.hide();
-            }
-        });
-    });
-});
+
